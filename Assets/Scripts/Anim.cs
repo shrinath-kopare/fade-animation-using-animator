abf17c760e0d0cf2aa7ddfdbbde6a08b79978cad
shrinath-kopare <shrinath.kopare@gmail.com>
2018-01-29 20:48:21 +0530

    Initial upload.

---
+++ Assets/Scripts/Anim.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Anim : MonoBehaviour {
+    public GameObject cg;
+    public RuntimeAnimatorController animator;
+    void Start () {
+        cg.gameObject.AddComponent<Animator>();
+        cg.gameObject.GetComponent<Animator>().runtimeAnimatorController = animator;
+    }
+	
+    void OnGUI() {
+        if (GUI.Button(new Rect(10, 70, 50, 30), "Fade")){
+            cg.gameObject.GetComponent<Animator>().SetTrigger("fade");
+        }
+    }
+}
